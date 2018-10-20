5a7591a76f1a9728bdc38abb0e66b007a53a6be7
Frognet Theotime <theotime.frognet@telecomnancy.net>
2018-10-21 00:06:59 +0200

    deux plateformes

--- src/verticalPlateformer/Plateforme.java
+++ src/verticalPlateformer/Plateforme.java
@@ -6,13 +6,13 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class Plateforme extends Rectangle {
+public abstract class Plateforme extends Rectangle {
 	private float posx;
 	private float posy;
 	private boolean sens; /* sens de la plateforme */
 	private float longueur;
 	private float epaisseur;
-
+	private float speed;
 	
 	
 	public void render (GameContainer container, StateBasedGame game, Graphics g,boolean gravite) {
@@ -29,7 +29,7 @@ public class Plateforme extends Rectangle {
 		}
 	}
 	
-	private Plateforme(float posx,float posy,float longueur,float epaisseur,boolean sens) {
+	public Plateforme(float posx,float posy,float longueur,float epaisseur,boolean sens) {
 		super(posx, posy, sens ? longueur : epaisseur, sens ? epaisseur : longueur );
 		this.posx=posx;
 		this.posy=posy;
@@ -37,7 +37,8 @@ public class Plateforme extends Rectangle {
 		this.epaisseur=epaisseur;
 		this.sens=sens;
 	}
-	
+	public abstract void update(GameContainer container, StateBasedGame game, int delta);
+
 	
 	 
 }
---
+++ src/verticalPlateformer/PlateformeMouvante.java
@@ -0,0 +1,14 @@
+package verticalPlateformer;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class PlateformeMouvante extends Plateforme {
+	private PlateformeMouvante(float posx,float posy,float longueur,float epaisseur,boolean sens) {
+		super(posx,posy,longueur,epaisseur,sens);
+		
+	}
+public void update(GameContainer container, StateBasedGame game, int delta) {
+		
+	}
+}
