41bf4069e32db748078565ce22f39c78b3e7356c
Amos George <amos.george0@gmail.com>
2018-10-20 19:14:06 +0200

    Initial commit

---
+++ src/pages/Choice.java
@@ -0,0 +1,53 @@
+package pages;
+
+import java.util.Arrays;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
+
+import app.AppMenu;
+import app.elements.MenuItem;
+
+public class Choice extends AppMenu {
+
+	public Choice (int ID) {
+		super (ID);
+	}
+
+	@Override
+	public void init (GameContainer container, StateBasedGame game) {
+		super.initSize (container, game, 600, 400);
+		super.init (container, game);
+		this.setSubtitle ("CN du 20 octobre 2018");
+		this.setMenu (Arrays.asList (new MenuItem [] {
+			new MenuItem ("HaxBall") {
+				public void itemSelected () {
+					((haxBall.World) game.getState (3)).setState (0);
+					game.enterState (3, new FadeOutTransition (), new FadeInTransition ());
+				}
+			},
+			new MenuItem ("Vertical Plateformer") {
+				public void itemSelected () {
+					((verticalPlateformer.World) game.getState (4)).setState (0);
+					game.enterState (3, new FadeOutTransition (), new FadeInTransition ());
+				}
+			},
+			new MenuItem ("Multigame") {
+				public void itemSelected () {
+					((multigame.World) game.getState (5)).setState (0);
+					game.enterState (3, new FadeOutTransition (), new FadeInTransition ());
+				}
+			},
+			new MenuItem ("Retour") {
+				public void itemSelected () {
+					game.enterState (0, new FadeOutTransition (), new FadeInTransition ());
+				}
+			}
+		}));
+		this.setTitle ("Choix");
+		this.setHint ("SELECT A GAME");
+	}
+
+}
