5b8f0ca6df73da8c19b3736af30b92c3d06fdfab
maxime botreau <maxime.botreau-roussel-bonneter@tlelcomnancy.eu>
2018-10-20 22:56:36 +0200

    Nouvelle Maj du terrain + render Ball

--- src/haxBall/Field.java
+++ src/haxBall/Field.java
@@ -13,17 +13,19 @@ public class Field {
 	private Color color;
 	private Player player0;
 	private Player player1;
+	private Ball ball;
 	
 	public Field(int world_height , int world_width){
 		// normalement ca marche (pas)...
-		this.height = (int)(0.6 * world_height);
-		this.width = (int)(0.6 * world_width);
-		this.pos_x = (int)(0.2 * world_width);
-		this.pos_y = (int)(0.2 * world_height);
+		this.height = (int)(0.7 * world_height);
+		this.width = (int)(0.7 * world_width);
+		this.pos_x = (int)(0.15 * world_width);
+		this.pos_y = (int)(0.15 * world_height);
 		
 		// creation des joueurs ...
 		player0 = new Player(this.height,this.width, 0);
 		player1 = new Player(this.height,this.width, 1);
+		ball = new Ball(world_height,world_width);
 	}
 	
 	public void setColor(Color c) {
@@ -37,9 +39,10 @@ public class Field {
 	
 	public void render (GameContainer container, StateBasedGame game, Graphics context) {
 		/* Méthode exécutée environ 60 fois par seconde on espère !  */
-		context.setColor(new Color(200,10,10));
+		context.setColor(new Color(102, 148, 68));
 		context.fillRect(this.pos_x,this.pos_y,this.width,this.height);
 		player0.render(container, game, context);
 		player1.render(container, game, context);
+		ball.render(container, game, context);
 	}
 }
