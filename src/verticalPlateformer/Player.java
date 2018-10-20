a335a56116bc52983c7ae506867dd7910f0e2d6b
Amos George <amos.george0@gmail.com>
2018-10-20 21:45:15 +0200

    Player

---
+++ src/verticalPlateformer/Player.java
@@ -0,0 +1,18 @@
+package verticalPlateformer;
+
+public class Player {
+	
+	private int horizontalSpeed;
+	private int vertivalSpeed;
+	private int horizontalAcceleration;
+	private int vertivalAcceleration;
+	private double posX;
+	private double posY;
+	
+	public Player() {
+		
+	}
+	
+	
+	
+}
