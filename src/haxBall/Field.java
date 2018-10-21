74e64cd4a8e88ae52a35fecefcdd1a645b3bea08
maxime botreau <maxime.botreau-roussel-bonneter@tlelcomnancy.eu>
2018-10-21 02:56:34 +0200

    Voilà le second terrain mdr jpp

--- src/haxBall/Field.java
+++ src/haxBall/Field.java
@@ -18,6 +18,7 @@ public class Field {
 	private Ball ball;
 	private int world_height;
 	private int world_width;
+	private float rnd;
 	
 	public Field(int world_height , int world_width){
 		// normalement ca marche (pas)...
@@ -28,7 +29,8 @@ public class Field {
 		this.color = new Color(102, 148, 68);
 		this.world_width = world_width;
 		this.world_height = world_height; 
-		
+		this.rnd = (float) Math.random();
+		System.out.println(this.rnd);
 		
 		// creation des joueurs ...
 		player0 = new Player(this.height,this.width,this.pos_x , this.pos_y, 0);
@@ -70,27 +72,50 @@ public class Field {
 	
 	public void render (GameContainer container, StateBasedGame game, Graphics context) {
 		/* Méthode exécutée environ 60 fois par seconde on espère !  */
-
-		context.setColor(new Color(102, 111, 69));
 		context.setColor(new Color(102, 111, 69));
 		context.fillRect(0, 0, this.world_width, this.world_height);
 		context.setColor(new Color(102, 148, 68));
 		context.fillRect(this.pos_x,this.pos_y,this.width,this.height);
-
-		context.setColor(new Color(155, 177, 181));// Buts
-		context.fillRect(this.pos_x - this.width/16 , this.pos_y+ this.height / 3, this.width / 16, this.height / 3);// but 1
-		context.fillRect(this.pos_x + this.width  , this.pos_y + this.height / 3, this.width / 16, this.height / 3); // but 2
-
-		context.setColor(new Color(243, 241, 255)); // traits inutiles
-		context.drawRect(this.pos_x ,  this.pos_y+ this.height/4,  this.width / 8, this.height / 2); // zone 1
-		context.drawRect(this.pos_x +  7 *this.width /8 ,  this.pos_y+ this.height/4,  this.width / 8, this.height / 2); // zone 2
-		context.drawLine(this.pos_x + this.width/2, this.pos_y, this.pos_x + this.width/2, this.pos_y + this.height); // ligne milieu
-		context.drawRect(this.pos_x, this.pos_y, this.width, this.height); // touche
-		context.drawOval(this.pos_x + this.width/2 - this.height/8 , this.pos_y + this.height/2 - this.height/8, height/4, height/4); // cercle
+		context.setColor(new Color(243, 241, 255));
+		context.drawRect(this.pos_x, this.pos_y, this.width, this.height);
+		if(this.rnd > 0.5) {
+			
+			context.setColor(new Color(155, 177, 181));// Buts
+			context.fillRect(this.pos_x - this.width/16 , this.pos_y+ this.height / 3, this.width / 16, this.height / 3);// but 1
+			context.fillRect(this.pos_x + this.width  , this.pos_y + this.height / 3, this.width / 16, this.height / 3); // but 2
+	
+			context.setColor(new Color(243, 241, 255)); // traits inutiles
+			context.drawRect(this.pos_x ,  this.pos_y+ this.height/4,  this.width / 8, this.height / 2); // zone 1
+			context.drawRect(this.pos_x +  7 *this.width /8 ,  this.pos_y+ this.height/4,  this.width / 8, this.height / 2); // zone 2
+			context.drawLine(this.pos_x + this.width/2, this.pos_y, this.pos_x + this.width/2, this.pos_y + this.height); // ligne milieu
+			context.drawRect(this.pos_x, this.pos_y, this.width, this.height); // touche
+			context.drawOval(this.pos_x + this.width/2 - this.height/8 , this.pos_y + this.height/2 - this.height/8, height/4, height/4); // cercle
+	
+			player0.render(container, game, context);
+			player1.render(container, game, context);
+			ball.render(container, game, context);
+		}
+		else {
+			context.setColor(new Color(231, 235, 221));
+			for(int i = 1 ; i < 6 ; i++) {
+				context.drawLine(this.pos_x + i * (this.width / 6)  , this.pos_y,this.pos_x + i * (this.width / 6) , this.pos_y  + this.height);
+			}
+			context.setColor(new Color(234, 217, 127));
+			context.drawRect(this.pos_x - this.width/20, this.pos_y + height/3- width/20, width/20, width/20);
+			context.drawRect(this.pos_x - this.width/20, this.pos_y + 2*height/3 , width/20, width/20);
+			context.drawRect(this.pos_x + this.width, this.pos_y + height/3- width/20, width/20, width/20);
+			context.drawRect(this.pos_x + this.width, this.pos_y + 2*height/3 , width/20, width/20);
+			context.setColor(new Color(255, 217, 127));
+			context.fillRect(this.pos_x - this.width/20, this.pos_y + height/3- width/20, width/20, width/20);
+			context.fillRect(this.pos_x - this.width/20, this.pos_y + 2*height/3 , width/20, width/20);
+			context.fillRect(this.pos_x + this.width, this.pos_y + height/3- width/20, width/20, width/20);
+			context.fillRect(this.pos_x + this.width, this.pos_y + 2*height/3 , width/20, width/20);
 
 		player0.render(container, game, context);
 		player1.render(container, game, context);
-		ball.render(container, game, context);}
+		ball.render(container, game, context);
+		}
+	}
 	
 	public void keyPressed(int key, char c) {
 		player0.keyPressed(key,c);
@@ -101,6 +126,6 @@ public class Field {
 	public void keyReleased(int key, char c) {
 		player0.keyReleased(key,c);
 		player1.keyReleased(key,c);
-		//ball.keyReleased(key,c);
+		//*ball.keyReleased(key,c);
 	}
 }
