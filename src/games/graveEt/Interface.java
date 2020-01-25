02641ee9748cd8bdac09d40a1674a4b4567f6556
stephane-hulot <hulot.stephane.55@gmail.com>
2020-01-25 15:50:47 +0100

    Amelioration de l'interface en jeu

--- src/games/graveEt/Interface.java
+++ src/games/graveEt/Interface.java
@@ -7,19 +7,38 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
+import java.awt.Font;
+import org.newdawn.slick.TrueTypeFont;
+
 public class Interface {
 	private ArrayList<Player> players;
+	private TrueTypeFont fontSmall;
+	private TrueTypeFont fontBig;
 
 	public Interface(ArrayList<Player> p) {
 		this.players = p;
+
+		fontSmall = new TrueTypeFont(new Font("Verdana", Font.PLAIN, 24), true);
+		fontBig = new TrueTypeFont(new Font("Verdana", Font.BOLD, 42), true);
 	}
 
 	public void render(GameContainer container, StateBasedGame game, Graphics g) {
 		// Affichage du score
+		fontSmall.drawString(10, 10, "Score");
+		fontBig.drawString(10, 30, Integer.toString(this.players.get(0).getScore()));
+
+		int pos = container.getWidth() - 230;
+		int points = this.players.get(0).getGravityPoint();
+		fontSmall.drawString(pos, 10, "Points de gravite");
+		Color col = points < 4 ? (points == 0 ? Color.black : Color.red) : Color.white;
+		fontBig.drawString(pos, 38, Integer.toString(points), col);
+		/*
 		g.setColor(Color.white);
 		for ( Player p : this.players) {
 			g.drawString("Score de "+p.getName()+ " : " + p.getScore(), 399, 10);
 			g.drawString("Nombre de points de Gravité restants à " + p.getName()+ " : " + p.getGravityPoint(), 700, 10);
 		}
+		*/
+
 	}
 }
